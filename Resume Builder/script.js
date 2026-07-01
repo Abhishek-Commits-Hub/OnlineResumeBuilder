6c43a8d0855fc83cc1d34dd956026984b9850c30
sourav191007 <kumarsourav191007@gmail.com>
2026-07-01 22:01:00 +0530

    js code add project button

--- Resume Builder/script.js
+++ Resume Builder/script.js
@@ -9,3 +9,14 @@ var mainButton=document.querySelector("#we");
 var insertButton=document.querySelector("#weAddButton");
 mainButton.insertBefore(tx,insertButton);
 }
+function addFieldProject(){
+   let tx=document.createElement("textarea");
+tx.classList.add('form-control');
+tx.classList.add('weProject');
+tx.classList.add('mt-2');
+tx.setAttribute("placeholder","Add Project");
+tx.setAttribute("rows",2);
+var mainButton=document.querySelector("#weProject fieldset");
+var insertButton=document.querySelector("#weAddButtonProject");
+mainButton.insertBefore(tx,insertButton); 
+}
