d18e11060a58dbdecc6732ed2f57efa6e2765c35
sourav191007 <kumarsourav191007@gmail.com>
2026-07-01 17:57:01 +0530

    Update script.js

--- Resume Builder/script.js
+++ Resume Builder/script.js
@@ -3,7 +3,7 @@ let tx=document.createElement("textarea");
 tx.classList.add('form-control');
 tx.classList.add('weField');
 tx.classList.add('mt-2');
-tx.setAttribute("placeholder","enter here");
+tx.setAttribute("placeholder","Add skill");
 tx.setAttribute("rows",2);
 var mainButton=document.querySelector("#we");
 var insertButton=document.querySelector("#weAddButton");
